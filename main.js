d5cbf47c6872103de939adf97367c2e348187625
kleosdc <64943294+kleosdc@users.noreply.github.com>
2023-03-26 19:12:31 -0400

    First Upload
    Test functionality

---
+++ main.js
@@ -0,0 +1,390 @@
+// Example list of strings
+/*VIDEO_ID: ["Album", "Song", "Date"] 64 in KV*/
+playlist = {
+  "iLXyoC2HRgY": ["Love Shake", "Love Shake", "September 22, 2014"],
+  "XwCDkRiJucw": ["Love Shake", "Shut Up", "September 22, 2014"],
+  "zihoyz0u_cs": ["Nightmare", "Chase Me", "January 13, 2017"],
+  "5ST_EJBhwO4": ["Nightmare", "Emotion", "January 13, 2017"],
+  "Lxfl8LRab_I": ["Fall asleep in the mirror", "GOOD NIGHT", "April 5, 2017"],
+  "rSq1URfEEC8": ["Fall asleep in the mirror", "Lullaby", "April 5, 2017"],
+  "39yeTdIuKJU": ["Prequel", "Fly High", "July 27, 2017"],
+  "vuYQhvoYXIQ": ["Prequel", "Wake Up", "July 27, 2017"],
+  "41lCjS3Ymx8": ["Prequel", "Sleep walking", "July 27, 2017"],
+  "Ecc0N9VBXqM": ["Full Moon", "Full Moon", "January 12, 2018"],
+  "I5_BQAtwHws": ["Escape the ERA", "YOU AND I", "May 10, 2018"],
+  "3iqEB5LlO2k": ["Escape the ERA", "Mayday", "May 10, 2018"],
+  "EhwX3y9Ty20": ["Escape the ERA", "Which a star", "May 10, 2018"],
+  "jmYCH6p7HPg": ["Escape the ERA", "Scar", "May 10, 2018"],
+  "pN0dkjp1deQ": ["Alone In The City", "What", "September 20, 2018"],
+  "iITO8xBvcHI": ["Alone In The City", "Wonderland", "September 20, 2018"],
+  "eoqwlzfyRSY": ["Alone In The City", "Trap", "September 20, 2018"],
+  "BO6rEpssQDY": ["Alone In The City", "July 7th", "September 20, 2018"],
+  "3F2nRTyVPGo": ["Over the Sky", "Over the Sky", "January 16, 2019"],
+  "Pq_mbTSR-a0": ["The End of Nightmare", "PIRI", "Febuary 13, 2019"],
+  "54j-ePPQStU": ["The End of Nightmare", "Diamond", "Febuary 13, 2019"],
+  "xjKn61YOHIk": ["The End of Nightmare", "And there was no one left", "Febuary 13, 2019"],
+  "X1Mte4dHW1s": ["The End of Nightmare", "Daydream", "Febuary 13, 2019"],
+  "W761DtH1oRg": ["Raid of Dream", "Deja Vu", "September 18, 2019"],
+  "5ycjCmDYD2I": ["Raid of Dream", "The curse of the Spider", "September 18, 2019"],
+  "ovw4Cy-zMiI": ["Raid of Dream", "Silent Night", "September 18, 2019"],
+  "jG5CwtPDGB8": ["Raid of Dream", "Polaris", "September 18, 2019"],
+  "FKlGHHhTOsQ": ["Dystopia: The Tree of Language", "Scream", "February 18, 2020"],
+  "-6_LfeObHPU": ["Dystopia: The Tree of Language", "Tension", "February 18, 2020"],
+  "RKBJVdjOHUY": ["Dystopia: The Tree of Language", "Red Sun", "February 18, 2020"],
+  "t3ptx0spMtQ": ["Dystopia: The Tree of Language", "Black Or White", "February 18, 2020"],
+  "Fo64mNT5GOA": ["Dystopia: The Tree of Language", "Jazz Bar", "February 18, 2020"],
+  "BgLhbkKu1s4": ["Dystopia: The Tree of Language", "SAHARA", "February 18, 2020"],
+  "okGlx-t9hXA": ["Dystopia: The Tree of Language", "In The Frozen", "February 18, 2020"],
+  "Um2tn8zM8bM": ["Dystopia: The Tree of Language", "Daybreak", "February 18, 2020"],
+  "MZ4JGye4dQU": ["Dystopia : Lose Myself", "BOCA", "August 17, 2020"],
+  "TqcPjwKU_zc": ["Dystopia : Lose Myself", "Break The Wall", "August 17, 2020"],
+  "F_2W8SW8iXA": ["Dystopia : Lose Myself", "Can’t get you out of my mind", "August 17, 2020"],
+  "nqrHHZjc_Rs": ["Dystopia : Lose Myself", "Dear", "August 17, 2020"],
+  "1QD0FeZyDtQ": ["Dystopia : Road to Utopia", "Odd Eye", "January 26, 2021"],
+  "CF0b5MDBJu0": ["Dystopia : Road to Utopia", "Wind Blows", "January 26, 2021"],
+  "wKXjvh-wYKY": ["Dystopia : Road to Utopia", "Poison Love", "January 26, 2021"],
+  "SGemMpImsXg": ["Dystopia : Road to Utopia", "4 Memory", "January 26, 2021"],
+  "Ag2AWsFY5Ns": ["Dystopia : Road to Utopia", "New Days", "January 26, 2021"],
+  "PEKkdIT8JPM": ["Summer Holiday", "BEcause", "July 30, 2021"],
+  "sNgM3uBLkEU": ["Summer Holiday", "Airplane", "July 30, 2021"],
+  "cTrWrRmHICw": ["Summer Holiday", "Whistle", "July 30, 2021"],
+  "AWemTDbxs14": ["Summer Holiday", "Alldaylong", "July 30, 2021"],
+  "5gvfp-haKXc": ["Summer Holiday", "A Heart of Sunflower", "July 30, 2021"],
+  "jZgfRnnKhlA": ["Apocalypse : Save Us", "Locked Inside A Door", "April 12, 2022"],
+  "z4t9LLq1Nk0": ["Apocalypse : Save Us", "Maison", "April 12, 2022"],
+  "chYPWJk6s6E": ["Apocalypse : Save Us", "Starlight", "April 12, 2022"],
+  "FFXtTiikOic": ["Apocalypse : Save Us", "Together", "April 12, 2022"],
+  "2L0q-nWS1xA": ["Apocalypse : Save Us", "Cherry", "April 12, 2022"],
+  "oxPyRRKEtHI": ["Apocalypse : Save Us", "No Dot", "April 12, 2022"],
+  "CowRTDlxbWg": ["Apocalypse : Save Us", "Entrancing", "April 12, 2022"],
+  "jQ_ogJWssXU": ["Apocalypse : Save Us", "Winter", "April 12, 2022"],
+  "1HwjxbE7_Qw": ["Apocalypse : Save Us", "For", "April 12, 2022"],
+  "nivI7w9HagI": ["Apocalypse : Save Us", "Beauty Full", "April 12, 2022"],
+  "iYvErpLnQ-8": ["Apocalypse : Save Us", "Playground", "April 12, 2022"],
+  "jKrJBVLnRiM": ["Apocalypse : Follow  Us", "Vision", "October 11, 2022"],
+  "bOLq9w-JSF0": ["Apocalypse : Follow  Us", "Fairytale", "October 11, 2022"],
+  "022GXgNuoc8": ["Apocalypse : Follow  Us", "Some Love", "October 11, 2022"],
+  "hdt-SIpJqFE": ["Apocalypse : Follow  Us", "Rainy Day", "October 11, 2022"],
+  "G3fUZUh1LBs": ["Reason", "Reason", "January 13, 2023"],
+}
+
+// Create an empty list for completed strings
+let completedList = {};
+
+let VIDEO_ID = '';
+let STARTED = false
+let START_TIME = 0;
+let START_TIME_SECONDS = 0;
+let END_TIME = 0;
+let skipped = 0;
+let CURRENT_SONG = '';
+let CURRENT_ALBUM = '';
+let CURRENT_RELEASE = '';
+
+function getSong() {
+  if (playlist.length == 0) {
+    console.log("No more songs")
+    nextButton.style.visibility = "hidden";
+    return;
+  }
+  let keys = Object.keys(playlist);
+  // Generate a random index within the range of the original list
+  let randomIndex = Math.floor(Math.random() * keys.length);
+  let randomKey = keys[randomIndex];
+
+  CURRENT_SONG = playlist[randomKey][1];
+  CURRENT_ALBUM = playlist[randomKey][0];
+  CURRENT_RELEASE = playlist[randomKey][2];
+  console.log(`Song: ${CURRENT_SONG} - Album: ${CURRENT_ALBUM} - Release: ${CURRENT_RELEASE}`);
+
+  completedList[randomKey] = playlist[randomKey];
+
+  // Get the randomly selected string from the original list
+  VIDEO_ID = keys[randomIndex];
+
+  delete playlist[randomKey];
+
+  // Print the updated lists
+  console.log("Original List:", playlist);
+  console.log("Completed List:", completedList);
+  getTimeframe();
+}
+
+function getTimeframe() {
+  // Get a random number of seconds between 25 and 150 (2 minutes and 30 seconds)
+  let randomSeconds1 = Math.floor(Math.random() * (150 - 25 + 1) + 25);
+  let randomSeconds2 = randomSeconds1 + 1;
+
+  // Convert the random number of seconds to Date objects
+  let time1 = new Date(randomSeconds1 * 1000);
+  let time2 = new Date(randomSeconds2 * 1000);
+
+  // Format the times as x:xx
+  START_TIME = time1.toISOString().substr(14, 5);
+  START_TIME_SECONDS = START_TIME.split(':').reduce((acc, val) => acc * 60 + parseInt(val, 10), 0);
+  END_TIME = time2.toISOString().substr(14, 5);
+
+  // Print the formatted times
+  console.log(START_TIME, END_TIME, START_TIME_SECONDS);
+}
+
+function gameCheck() {
+  if (skipped > 5) {
+    console.log('Game Over');
+    alert('Game Over');
+    resetButton.click();
+    console.log("Auto Restart!");
+    return false
+  }
+  return true
+}
+
+function resetGame() {
+  Object.assign(playlist, completedList);
+
+  for (const key in completedList) {
+    if (completedList.hasOwnProperty(key)) {
+      delete completedList[key];
+    }
+  }
+  nextButton.style.visibility = "visible";
+  nextButton.click();
+}
+
+function updatePlayer() {
+  if (gameCheck()) {
+    youTubePlayerChangeVideoId(videoId, minutesToSeconds(START_TIME), minutesToSeconds(END_TIME));
+    displayStats();
+    STARTED = true;
+  }
+}
+
+function gameStatus() {
+  if (Object.keys(completedList).length == 0 && !(STARTED)) {
+    alert('Game has not started, press "Start" then "Play"');
+    return false
+  }
+  return true
+}
+
+function displayStats() {
+  console.log(`Songs remaining: ${Object.keys(playlist).length} : Complete: ${Object.keys(completedList).length} : Timeframe: ${START_TIME}-${END_TIME}`);
+}
+
+function loadData() {
+  // Create an empty array to hold the <li> elements
+  let liArr = [];
+
+  // Loop through each key in the object
+  for (let key in playlist) {
+    // Create a new <li> element
+    const li = document.createElement("li");
+
+    // Concatenate the two values using the array syntax
+    let value = playlist[key][1];
+
+    // Set the innerHTML of the <li> element to the concatenated values
+    li.innerHTML = value;
+
+    // Add the <li> element to the array
+    liArr.push(li);
+  }
+
+  // Shuffle the array using Array.sort() and Math.random()
+  liArr.sort(() => Math.random() - 0.5);
+
+  // Add the shuffled <li> elements to the <ul> element
+  liArr.forEach(li => songColumn.appendChild(li));
+}
+
+
+var player;
+
+let nextButton = document.getElementById("nextButton");
+let timeButton = document.getElementById("timeButton");
+let resetButton = document.getElementById("resetButton");
+
+let songColumn = document.getElementById("songColumn");
+let albumColumn = document.getElementById("albumColumn");
+let releaseColumn = document.getElementById("releaseColumn");
+
+let imageHolder = document.getElementById("imageHolder");
+let playerHolder = document.getElementById("player-container");
+
+let input = document.getElementById("myInput");
+
+loadData();
+
+songColumn.addEventListener("click", (e) => {
+  if (e.target.tagName == "LI" && gameStatus()) {
+    console.log(e.target.textContent, CURRENT_SONG);
+    if (e.target.textContent == CURRENT_SONG) {
+      youTubePlayerChangeVideoId(videoId, START_TIME_SECONDS, (START_TIME_SECONDS+30));
+      
+      setTimeout(() => { playVideo(); }, 1000);
+      imageHolder.style.display = "none";
+      playerHolder.style.display = "block";
+      console.log('Correct song.');
+    } else {
+      console.log('Wrong song selected.');
+    }
+  }
+});
+
+nextButton.addEventListener("click", (e) => {
+  if (Object.keys(playlist).length == 0) {
+    alert('No more songs');
+    return
+  }
+  // Check if game has started
+  if (!(gameStatus())) {
+    return
+  }
+  if (document.getElementsByClassName('playButton')[0].textContent != "Start") {
+    imageHolder.style.display = "block";
+    playerHolder.style.display = "none";
+    e.preventDefault();
+    getSong();
+    skipped = 0;
+    videoId = YouTubeGetID("https://www.youtube.com/watch?v="+VIDEO_ID);
+    updatePlayer();
+    setTimeout(() => { playVideo(); }, 1000);
+  }
+});
+
+timeButton.addEventListener("click", (e) => {
+  // Game unplayable after 5 skips
+  if (!(gameCheck())) {
+    return
+  }
+  // Check if game has started
+  if (!(gameStatus())) {
+    return
+  }
+
+  skipped += 1;
+  console.log(END_TIME);
+  e.preventDefault();
+  let timeStr = `${Math.floor(((START_TIME_SECONDS+skipped)+((skipped+1))) / 60).toString().padStart(2, "0")}:${(((START_TIME_SECONDS+skipped)+((skipped+1))) % 60).toString().padStart(2, "0")}`;
+  END_TIME = timeStr;
+  console.log(END_TIME, skipped, ((START_TIME_SECONDS+1)+((skipped+1))));
+  updatePlayer();
+});
+
+resetButton.addEventListener("click", (e) => {
+  resetGame();
+});
+
+function youTubePlayerCurrentTimeChange(currentTime) {
+
+    player.currentTimeSliding = false;
+        player.seekTo(currentTime*player.getDuration()/100, true);
+    
+}
+
+function youTubePlayerVolumeChange(volume) {
+        player.setVolume(volume);
+}
+
+function youTubePlayerCurrentTimeSlide() {
+
+    player.currentTimeSliding = true;
+}
+
+function pauseVideo() {
+  player.pauseVideo();
+}
+function playVideo() {
+  if (gameCheck() == "Game Over") {
+    return
+  }
+  if (document.getElementsByClassName('playButton')[0].textContent == "Start") {
+    getSong();
+    skipped = 0;
+    videoId = YouTubeGetID("https://www.youtube.com/watch?v="+VIDEO_ID);
+    youTubePlayerChangeVideoId(videoId, minutesToSeconds(START_TIME), minutesToSeconds(END_TIME));
+
+    document.getElementsByClassName('playButton')[0].textContent = "Play";
+  }
+  player.playVideo();
+  player.seekTo(START_TIME_SECONDS);
+}
+
+function YouTubeGetID(url) {
+  var ID = "";
+  url = url
+    .replace(/(>|<)/gi, "")
+    .split(/(vi\/|v=|\/v\/|youtu\.be\/|\/embed\/)/);
+  if (url[2] !== undefined) {
+    ID = url[2].split(/[^0-9a-z_\-]/i);
+    ID = ID[0];
+  } else {
+    ID = url;
+  }
+  return ID;
+}
+
+function youTubePlayerChangeVideoId(videoId, starttime, endtime) {
+  player.cueVideoById({ suggestedQuality: "tiny", videoId: videoId, startSeconds: starttime, endSeconds: endtime });
+  pauseVideo();
+}
+
+function onYouTubeIframeAPIReady() {
+  player = new YT.Player("player", {
+    height: 270,
+    width: 480,
+    videoId: "",
+    playerVars: {
+      playsinline: 1,
+      autoplay: 1,
+      controls: 1,
+    },
+    events: {
+      'onReady': function (event) {
+        event.target.setVolume(40);
+        onPlayerReady;
+      },
+      onStateChange: onPlayerStateChange,
+    },
+  });
+}
+
+
+function onPlayerReady() {
+  console.log(true);
+}
+
+var done = false;
+function onPlayerStateChange(event) {
+  if (event.data == YT.PlayerState.PLAYING && !done) {
+    done = true;
+  }
+}
+
+function minutesToSeconds(mins) {
+      /**
+       * ~~ mins param must be a str in "1:00" format ~~
+       * Converts time in xx:xx format to seconds
+       */
+
+      let p = mins.split(":");
+      return Number((Number(p[0]) * 60 + Number(p[1])).toFixed(3));
+}
+
+function filterList() {
+  var filter = input.value.toLowerCase();
+  
+  // Get the list and all list items
+  //var list = document.getElementById("myList");
+  var items = songColumn.getElementsByTagName("li");
+  
+  // Loop through all list items, and hide those that don't match the search query
+  for (var i = 0; i < items.length; i++) {
+    var item = items[i];
+    if (item.innerHTML.toLowerCase().indexOf(filter) > -1) {
+      item.style.display = "";
+    } else {
+      item.style.display = "none";
+    }
+  }
+}
